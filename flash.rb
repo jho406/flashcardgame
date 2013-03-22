0a56e4ebcc33e17fded1a8f24f60e25ba075641e
Apprentice <apprentice@dbc07.local>
2013-03-22 16:10:54 -0700

    add file load feature

--- flash.rb
+++ flash.rb
@@ -1,4 +1,5 @@
 require_relative 'ui'
+require 'csv'
 class Game
   attr_reader :deck
   def initialize(deck)
@@ -9,6 +10,17 @@ class Game
     term == deck.first.term
   end
 
+end
+
+module Deck
+  def self.load
+    cards = []
+    File.read("deck.txt").split("\n").each_slice(3) do |lines|
+      card = lines.select{|word|word != ""}
+      cards << {:term=>card.last, :definition=> card.first}
+    end
+    cards.map{|card| Card.new(card)}
+  end
 end
 
 class Card
@@ -22,15 +34,21 @@ class Card
 end
 
 
-card1 = Card.new({:term=> "alias", :definition => "To create a second name for the variable or method."})
-card2 = Card.new({:term=> "dbc", :definition => "school"})
+# card1 = Card.new({:term=> "alias", :definition => "To create a second name for the variable or method."})
+# card2 = Card.new({:term=> "dbc", :definition => "school"})
 
+# input = gets.chomp
+# # puts 
+# deck = [card1, card2]
+# game = Game.new(deck)
 
-input = gets.chomp
-# puts 
-deck = [card1, card2]
-game = Game.new(deck)
+# puts game.guess?(input)
 
-puts game.guess?(input)
+# File.read("deck.txt").split("\n").each_slice(3) do |lines|
+#   card = lines.select{|word|word != ""}
 
+# end
 
+game = Game.new(Deck.load)
+p game.guess?("alias")
+p game.deck.first
